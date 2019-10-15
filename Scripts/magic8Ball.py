0cb2b841cc86a4d26471f1eb1f8a65edb7449c56
liamplm <liamplm82@gmail.com>
2019-10-15 16:26:37 +0330

    optimaze magic8Ball

--- Scripts/magic8Ball.py
+++ Scripts/magic8Ball.py
@@ -1,27 +1,24 @@
 #!/usr/local/bin/python3
-#return statement
+# return statement
 import random
-def getAnswer(answerNumber):
-    if answerNumber == 1:
-        return 'It is certain'
-    elif answerNumber == 2:
-        return 'It is decidedly so'
-    elif answerNumber == 3:
-        return 'Yes'
-    elif answerNumber == 4:
-        return 'Reply hazy try again'
-    elif answerNumber == 5:
-        return 'Ask again later'
-    elif answerNumber == 6:
-        return 'Concentrate and ask again'
-    elif answerNumber == 7:
-        return 'My reply is no'
-    elif answerNumber == 8:
-        return 'Outlook not so good'
-    elif answerNumber == 9:
-        return 'Very doubtful'
 
-r = random.randint(1,9)
+
+possibleAnswer = [
+    'It is certain',
+    'It is decidedly so',
+    'Yes',
+    'Reply hazy try again',
+    'Ask again later',
+    'Concentrate and ask again',
+    'My reply is no',
+    'Outlook not so good',
+    'Very doubtful'
+]
+
+def getAnswer(answerIndex):
+    return possibleAnswer[answerIndex]
+
+
+r = random.randint(0, 8)
 fortune = getAnswer(r)
 print(fortune)
-
