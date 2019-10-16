6712bc2fe3842a6d37b672c9c70c2243b95390b8
sourabbr <sourabbr@gmail.com>
2019-10-16 14:17:35 +0530

    Added binary search to Script

---
+++ Scripts/binary_search.py
@@ -0,0 +1,20 @@
+def binarySearch (arr, l, r, x): 
+    if (r >= l): 
+        mid =int( l + (r - l)/2)
+        if (arr[mid] == x): 
+            return mid 
+        elif (arr[mid] > x): 
+            return binarySearch(arr, l, mid-1, x)  
+        else: 
+            return binarySearch(arr, mid+1, r, x) 
+    else: 
+        return -1
+  
+# Test array 
+arr = [23, 43, 47, 104, 402] 
+x = 104
+result = binarySearch(arr, 0, len(arr)-1, x)   
+if (result != -1): 
+    print ("Element is present at index %d" % result) 
+else: 
+    print ("Element is not present in array")
