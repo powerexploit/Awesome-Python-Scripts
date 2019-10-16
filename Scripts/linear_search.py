6a9ca478e0da47c8a0f9db7140c642546c75d22c
sahilverma0696 <sahilverma0696@gmail.com>
2019-10-16 14:34:33 +0530

    added linear search

---
+++ Scripts/linear_search.py
@@ -0,0 +1,21 @@
+# Python3 code to linearly search x in arr[]. 
+# If x is present then return its location, 
+# otherwise return -1 
+
+def search(arr, n, x): 
+
+	for i in range (0, n): 
+		if (arr[i] == x): 
+			return i; 
+	return -1; 
+
+# Driver Code 
+arr = [ 2, 13, 4, 10, 99 ]; 
+x = 10; 
+n = len(arr); 
+result = search(arr, n, x) 
+if(result == -1): 
+	print("Element is not present in array") 
+else: 
+	print("Element is present at index", result); 
+
