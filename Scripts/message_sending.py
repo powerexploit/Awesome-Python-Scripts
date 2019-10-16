410543ee1117754e2da960e999d893b02c2ce3eb
sahilverma0696 <sahilverma0696@gmail.com>
2019-10-16 14:49:34 +0530

    Removed message_sending script

--- Scripts/message_sending.py
+++
@@ -1,8 +0,0 @@
-# Sending automated message through the python script  
-import keyboard
-i =0
-while(i<11):
-    i+=1
-    keyboard.wait('enter')
-    keyboard.write("Sending message {} times ".format(i))
-    keyboard.send('enter')
