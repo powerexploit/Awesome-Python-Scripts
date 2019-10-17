bc18ff45776e2a0a64712d5c97a623a4e496329a
liamplm <liamplm82@gmail.com>
2019-10-17 20:48:49 +0330

    new feture: you can get random number in range n times

--- Scripts/rand_range.py
+++ Scripts/rand_range.py
@@ -3,11 +3,22 @@
 import random
 import sys
 
+print('----------------------------------------')
 try:
-    rand_min, rand_max = int(sys.argv[1]), int(sys.argv[2])
+    rand_min, rand_max, n = int(sys.argv[1]), int(sys.argv[2]), ( int(sys.argv[3]) if len(sys.argv) > 3 else 1 )
 
-    random_int = random.randint(rand_min, rand_max)
-    print('random number:', random_int)
+    random_ints = []
+    
+    if n == 1:
+        print('random number:', random.randint(rand_min, rand_max))
+    else:
+        for i in range(n):
+            random_int = random.randint(rand_min, rand_max)
+            print('\trandom number:', random_int)
+            random_ints.append(random_int)
+
+        print('----------------------------------------')
+        print(f'min: {min(random_ints)}, max: {max(random_ints)}, avg: {sum(random_ints)/len(random_ints)}')
 
 except ValueError:
     print('only number like value are valid for min, max')
