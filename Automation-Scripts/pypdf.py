92592fd6567767694610b3b2ccc90f2cea7645c6
Ankit Dobhal <dobhal.ankit@protonmail.com>
2019-10-24 18:38:31 +0530

    Create pypdf.py

---
+++ Automation-Scripts/pypdf.py
@@ -0,0 +1,14 @@
+#!/usrbin/python3
+# automation with pdf file
+# Script design just to automate stuffs with pdf file
+import PyPDF2
+import os
+print(os.system('pyfiglet pypdf'))
+pdFileObj = open('Exam-Transcript.pdf','rb')
+#open pdf file in read binary mode(rb) & store in pdfFileObj
+pdfReader = PyPDF2.PdfFileReader(pdFileObj)
+#pdfFileReader represents pdf file
+print(pdfReader.numPages)
+#to check number of pages in you pdf file use .numPages()
+pageObj = pdfReader.getPage(0)
+print(pageObj.extractText()) #extract the pdf data in text format
