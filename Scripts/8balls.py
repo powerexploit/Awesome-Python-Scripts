78bb28d6d587e67bdddd2c79d14983f73b6c6341
anisha282000 <mamgainanisha@gmail.com>
2020-05-28 21:20:34 +0530

    changes in file

--- Scripts/8balls.py
+++ Scripts/8balls.py
@@ -1,32 +1,26 @@
 b1,b2,b3,b4,b5,b6,b7,b8= input("enter weight of 8 balls").split()     #this will take input from the user and assign those values to the variables
 group=float(b1)+float(b2)+float(b3)+float(b4)
 group1=float(b5)+float(b6)+float(b7)+float(b8)
-group2 = float(b1)+float(b2)
-group3 =float(b3)+float(b4)
-group4 = float(b5)+float(6)
-group5 =float(b7)+float(b8)
 if group>group1:         
-    group2>group3
     if b1>b2:
      print("b1 is the different ball")
     elif b2>b1:
      print("b2 is the different ball")
     elif b3>b4:
-      print("b3 is the different ball")
+     print("b3 is the different ball")
     elif b4>b3:
-      print("b4 is the different ball")
+     print("b4 is the different ball")
+    elif group1>group:
+     b5>b6
+    print("b5 is the different ball")
+elif b6>b5:
+     print("b6 is the different ball")
+elif b7>b8:
+     print("b7 is the different ball")
+elif b8>b7:
+     print("b8 is the different ball")
 else:
-    group4>group5
-    if b5>b6:
-        print("b5 is the different ball")
-    elif b6>b5:
-        print("b6 is the different ball")
-    elif b7>b8:
-        print("b7 is the different ball")
-    elif b8>b7:
-        print("b8 is the different ball")
-    else:
-        print("NO different ball")
+     print("Invalid Input")
 
          
 
