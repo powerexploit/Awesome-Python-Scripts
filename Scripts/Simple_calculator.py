f54349fc334cd8b9cffaaaa4796340e631669be4
ritik soni <47344024+ritiksoni00@users.noreply.github.com>
2020-07-08 17:29:33 +0530

    Update Simple_calculator.py

--- Scripts/Simple_calculator.py
+++ Scripts/Simple_calculator.py
@@ -1,37 +1,29 @@
-#first define the functions
-#for 2 digits oprations
-def add(x, y):
-    return x+y
-def sub(x,y):
-    return x-y
-def mul(x,y):
-    return x*y
-def div(x,y):
-    return x/y
-def power(x,y):
-    return pow(x, y)
+operation = input('''
+Please type in the math operation you would like to complete:
+1 for addition
+2 for subtraction
+3 for multiplication
+4 for division
+''')
 
-#take input from user
-print("Select opration:")
-print("1.add")
-print("2.subtract")
-print("3.multiply")
-print("4.divide")
-print("5.power")
-select=input("Enter any one Choice from 1 to 5 :")
+num = int(input('Enter your first number: '))
+num2 = int(input('Enter your second number: '))
 
-num1=int(input("Enter the Num1:"))
-num2=int(input("Enter the Num2:"))
+if operation == '1':
+    print('{} + {} = '.format(num, num2))
+    print(num+num2)
 
-if select=="1":
-    print(num1,"+",num2,"=", add(num1,num2))
-elif select=="2":
-    print(num1,"-",num2,"=", sub(num1,num2))
-elif select=="3":
-    print(num1,"x",num2,"=", mul(num1,num2))
-elif select=="4":
-    print(num1,"/",num2,"=", div(num1,num2))
-elif select=="2":
-    print(num1,"*",num2,"=", power(num1,num2))
-else :
-    print("Not a valid input! tru again")
+elif operation == '2':
+    print('{} - {} = '.format(num, num2))
+    print(num-num2)
+
+elif operation == '3':
+    print('{} * {} = '.format(num, num2))
+    print(num*num2)
+
+elif operation == '4':
+    print('{} / {} = '.format(num, num2))
+    print(num / num2)
+
+else:
+    print('Wrong Input! Please Try Again')
