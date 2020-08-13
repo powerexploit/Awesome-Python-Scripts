61eda6d0d980896f7043e0eb033f7f7844ec9e35
Arju Aman <33257286+arjuaman@users.noreply.github.com>
2020-08-14 00:50:05 +0530

    Amazon Reviews Scraper
    Using scrapy, it scrapes the reviews from amazon.
    Links can be changed from myBaseUrl variable amazon_reviews_scraping/spider/scraper.py
    
    Run this in terminal to get the reviews in a csv file: 
    >>scrapy runspider amazon_reviews_scraping/amazon_reviews_scraping/spiders/scraper.py -o reviews.csv

---
+++ Web-Scraping/amazon_reviews_scraping/spiders/scraper.py
@@ -0,0 +1,36 @@
+import scrapy
+ 
+# Creating a new class to implement Spide
+class AmazonReviewsSpider(scrapy.Spider):
+ 
+    # Spider name
+    name = 'amazon_reviews'
+ 
+    # Domain names to scrape
+    allowed_domains = ['amazon.in']
+ 
+    # Base URL for the MacBook air reviews
+    myBaseUrl = "https://www.amazon.com/OnePlus-Interstellar-Unlocked-Android-Smartphone/product-reviews/B0872473BF/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews"
+    start_urls=[]
+ 
+    # Creating list of urls to be scraped by appending page number a the end of base url
+    for i in range(1,121):
+        start_urls.append(myBaseUrl+str(i))
+ 
+    # Defining a Scrapy parser
+    def parse(self, response):
+            data = response.css('#cm_cr-review_list')
+ 
+            # Collecting product star ratings
+            star_rating = data.css('.review-rating')
+ 
+            # Collecting user reviews
+            comments = data.css('.review-text-content')
+            count = 0
+ 
+            # Combining the results
+            for review in star_rating:
+                yield{'stars': ''.join(review.xpath('.//text()').extract()),
+                      'comment': ''.join(comments[count].xpath(".//text()").extract())
+                     }
+                count=count+1
