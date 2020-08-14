0cf85b7ee8ae1461266cc02462d1e119e844bd42
Yash Patel <49101492+ryuk156@users.noreply.github.com>
2020-08-15 00:59:02 +0530

    created Basic_OpenCV_Image_Processing.py

---
+++ Image-Processing/Basic_OpenCV_Image_Processing.py
@@ -0,0 +1,22 @@
+##Installation
+
+##Windows
+##$ pip install opencv-python
+##MacOS
+##$ brew install opencv3 --with-contrib --with-python3
+##Linux
+##$ sudo apt-get install libopencv-dev python-opencv
+
+
+##import the module i.e opencv
+import cv2
+
+##Reading Images and Finding Image details
+
+
+img = cv2.imread('rose.jpg') ##imread() is function use to read image which accepts iamge path
+
+##Image details
+print("- Number of Pixels: " + str(img.size))  ## .size will show image size 
+print("- Shape/Dimensions: " + str(img.shape)) ## .shape will show image shape/dimensions 
+print("- Datatype: " + str(img.dtype))  ##unit8
