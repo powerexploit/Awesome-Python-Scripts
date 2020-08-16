3919f46a4c31a2892dd5b45f91cd13a7eade18b1
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2020-08-16 10:00:18 +0530

    Add files via upload

---
+++ System-Automation-Scripts/qrGenerator.py
@@ -0,0 +1,20 @@
+import os
+import qrcode
+from sys import argv
+from urllib.parse import urlparse
+import random
+
+if len(argv)==2:
+  url =  str(argv[1])
+else:
+  url = input("Enter an URL to Encode in QR: ")
+
+path = ((urlparse(url)).netloc)
+image = qrcode.make(url) 
+
+if os.path.exists(f"{path}.png"):
+  path = path + str(random.randint(0,1000))
+
+image.save(f"{path}.png", "PNG")
+file_path = os.path.abspath(path)
+print("File saved to:", file_path)
