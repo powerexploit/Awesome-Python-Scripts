feec0b03e6e6568491770f737c9649f9e897fab2
Nagasuruthika N <66667949+Nagasuruthika@users.noreply.github.com>
2020-08-16 22:35:43 +0530

    Script to print a christmas tree pattern

---
+++ Basic-Scripts/Pattern.py
@@ -0,0 +1,17 @@
+picture = [
+  [0,0,0,1,0,0,0],
+  [0,0,1,1,1,0,0],
+  [0,1,1,1,1,1,0],
+  [1,1,1,1,1,1,1],
+  [0,0,0,1,0,0,0],
+  [0,0,0,1,0,0,0]
+]
+
+
+for image in picture:
+  for pixel in image:
+    if (pixel):
+      print('*', end ="")
+    else:
+      print(' ', end ="")
+  print('')
