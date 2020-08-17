07a3abde8a5b637e790f5bd2ffb744734d008727
namrun <nowitsharder@gmail.com>
2020-08-17 13:54:13 +0530

    Updated price_tracker.py

--- System-Automation-Scripts/Amazon-price-tracker/price_tracker.py
+++ System-Automation-Scripts/Amazon-price-tracker/price_tracker.py
@@ -72,47 +72,49 @@ def price_check(URL, budget):
         price_now.append(price)
     return(feasible, price_now)
 
-#Enter the URL of the amazon product
-URL = input("Enter the URL of the Amazon product : ")
 
-print("The price of the product is expressed in " + (currency_used(URL)))
+if __name__ == "__main__":
+    #Enter the URL of the amazon product
+    URL = input("Enter the URL of the Amazon product : ")
 
-#Enter your budget in the same currency as mentioned above
-budget = int(input("Enter your budget for the product in same unit of currency as mentioned above: "))
+    print("The price of the product is expressed in " + (currency_used(URL)))
 
-print("Please enter your email details as asked, when the price of the product is below or equal to your budget, you will receive an email conformation")
+    #Enter your budget in the same currency as mentioned above
+    budget = int(input("Enter your budget for the product in same unit of currency as mentioned above: "))
 
-#Since a mail is sent from this script, security mode has to be disabled
-print("Also ensure that security mode is switched off in your email settings")
+    print("Please enter your email details as asked, when the price of the product is below or equal to your budget, you will receive an email conformation")
 
-sender_email = input("Enter the sender's Email-ID : ")
-receiver_email = input("Enter the receiver's Email-ID : ")
-password = input("Enter the sender's password : ")
+    #Since a mail is sent from this script, security mode has to be disabled
+    print("Also ensure that security mode is switched off in your email settings")
 
-feasible, price_range = price_check(URL, budget)
+    sender_email = input("Enter the sender's Email-ID : ")
+    receiver_email = input("Enter the receiver's Email-ID : ")
+    password = input("Enter the sender's password : ")
 
-#The price_check function will execute every 12 hours to check, the prices will also be logged so that a comparison can be made
-while not feasible:
     feasible, price_range = price_check(URL, budget)
-    
-    if len(price_range) == 1: 
-        cost = "The cost of the product is  " + str(price_range[0])
-    else:
-        cost = "The cost of the product is within the range " + str(price_range[0]) + " and " + str(price_range[1])
-    
-    current_time = datetime.datetime.now()  
-    
-    #Logging records into the text file.
-    with open("price_logger.txt" , "w") as file:
-        file.write(cost + " at " + str(current_time))
-        file.write("\n")
-    
-    if feasible:
-        break
-    else:
-        #Sleeps for 12 hours and then checks for the same
-        time.sleep(43200)
 
-#The user will be notified through an email
-send_confirmation(sender_email, receiver_email, password, price_range)
+    #The price_check function will execute every 12 hours to check, the prices will also be logged so that a comparison can be made
+    while not feasible:
+        feasible, price_range = price_check(URL, budget)
+    
+        if len(price_range) == 1: 
+            cost = "The cost of the product is  " + str(price_range[0])
+        else:
+            cost = "The cost of the product is within the range " + str(price_range[0]) + " and " + str(price_range[1])
+    
+        current_time = datetime.datetime.now()  
+    
+        #Logging records into the text file.
+        with open("price_logger.txt" , "w") as file:
+            file.write(cost + " at " + str(current_time))
+            file.write("\n")
+    
+        if feasible:
+            break
+        else:
+            #Sleeps for 12 hours and then checks for the same
+            time.sleep(43200)
+
+    #The user will be notified through an email
+    send_confirmation(sender_email, receiver_email, password, price_range)
 
