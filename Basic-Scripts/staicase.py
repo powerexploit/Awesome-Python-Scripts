8505437150702e23e9e25f0ffc6157d7d7b3998b
Adityanagraj <42292430+Adityanagraj@users.noreply.github.com>
2020-08-17 12:55:33 +0530

    added main function and updated the variables name

--- Basic-Scripts/staicase.py
+++ Basic-Scripts/staicase.py
@@ -10,6 +10,9 @@ def staircase(height):
    for i in range(1,height+1):
        print(' '*(height-i) + '#'*i)
       
-print('Please enter the height of the staircase')      
-height=int(input()) #which accepts the input
-staircase(height)
+      
+if __name__ == '__main__':
+
+   print('Please enter the height of the staircase')      
+   height=int(input()) #which accepts the input
+   staircase(height)
