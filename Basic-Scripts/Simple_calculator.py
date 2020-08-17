5c2294593f9e42091037cc649550fe249e532f6e
muskangoyal06 <muskan06goyal98@gmail.com>
2020-08-18 00:31:30 +0530

    Update Simple_calculator.py

--- Basic-Scripts/Simple_calculator.py
+++ Basic-Scripts/Simple_calculator.py
@@ -1,35 +1,3 @@
-operation = input('''
-Please type in the math operation you would like to complete:
-1 for addition
-2 for subtraction
-3 for multiplication
-4 for division
-''')
-
-num = int(input('Enter your first number: '))
-num2 = int(input('Enter your second number: '))
-
-if operation == '1':
-    print('{} + {} = '.format(num, num2))
-    print(num+num2)
-
-elif operation == '2':
-    print('{} - {} = '.format(num, num2))
-    print(num-num2)
-
-elif operation == '3':
-    print('{} * {} = '.format(num, num2))
-    print(num*num2)
-
-elif operation == '4':
-    print('{} / {} = '.format(num, num2))
-    print(num / num2)
-
-else:
-    print('Wrong Input! Please Try Again")
-
-
-
 
 
 """The previous script becomes very boring for the user as he has to keep on typing just numbers and of wants to interact using 
