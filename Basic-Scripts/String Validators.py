7dc749fcb3c05a1f4a5b69a0e843186053a07f6a
Shashwat Mishra <38106962+shashwat397@users.noreply.github.com>
2020-08-17 10:01:39 +0530

    Update String Validators.py

--- Basic-Scripts/String Validators.py
+++ Basic-Scripts/String Validators.py
@@ -1,24 +1,15 @@
 #This is the program to find out if the string  contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
-if __name__ == '__main__':
-    s = input()
-    res = ["False", "False", "False", "False", "False"]
-    for i in s:
-        if i.isalnum():
-            res[0] = "True"
-        if i.isalpha():
-            res[1] = "True"
-        if i.isdigit():
-            res[2] = "True"
-        if i.islower():
-            res[3] = "True"
-        if i.isupper():
-            res[4] = "True"
-    print(*res, sep="\n")
+S = input()
+print(any(char.isalnum() for char in S))
+print(any(char.isalpha() for char in S))
+print(any(char.isdigit() for char in S))
+print(any(char.islower() for char in S))
+print(any(char.isupper() for char in S))
 
-    #Input Format will be A single line containig a string.
-    #Output Format will be as follows:
-    #In the first line, print True if string has any alphanumeric characters. Otherwise, print False.
-    #In the second line, print True if string  has any alphabetical characters. Otherwise, print False.
-    #In the third line, print True if string  has any digits. Otherwise, print False.
-    #In the fourth line, print True if string has any lowercase characters. Otherwise, print False.
-    #In the fifth line, print True if string  has any uppercase characters. Otherwise, print False.
+#Check the string  by using the methods: .isalnum(), .isalpha(), .isdigit(), .islower() and .isupper().
+
+#str.isalnum() : This method checks if all the characters of a string are alphanumeric (a-z, A-Z and 0-9).
+#str.isalpha() : This method checks if all the characters of a string are alphabetical (a-z and A-Z).
+#str.isdigit() : This method checks if all the characters of a string are digits (0-9).
+#str.islower() : This method checks if all the characters of a string are lowercase characters (a-z).
+#str.isupper() : This method checks if all the characters of a string are uppercase characters (A-Z)
