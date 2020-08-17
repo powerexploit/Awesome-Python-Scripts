ab96ffe111708355d801d99d266846d4dc98f7be
snehagupta <44.sneha.gupta.446@gmail.com>
2020-08-17 15:51:21 +0530

    Added Tic Tac Toe game code in python

--- Basic-Scripts/String_Manipulate.py
+++ Basic-Scripts/String_Manipulate.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 print('How are you?')
 feeling = input()
-if feeling.lower()=='great':
-    print('I feel great too!')
-else:
-    print('I hope the rest of day is good!')
+#feelings = ['great', 'happy', 'sad', 'fine', 'excited']
+#if feeling.lower()=='great':
+    #print('I feel great too!')
+#else:
+    #print('I hope the rest of day is good!')
+print(" I hope you stay "+ feeling + "forever")
