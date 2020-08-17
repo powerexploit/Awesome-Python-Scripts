e262d3f45742177626b73c20ed6ceeb4275c9295
GULSHAN BARAIK <45667577+gulshanbaraik01@users.noreply.github.com>
2020-08-17 14:24:49 +0530

    Update Rock_paper_scissor.py

--- Basic-Scripts/Rock_paper_scissor.py
+++ Basic-Scripts/Rock_paper_scissor.py
@@ -4,7 +4,7 @@ Created on Fri Sep 14 21:32:23 2018
 
 @author: Gulshan
 """
-
+# Function rock_paper_scissor--->
 def rock_paper_scissor(num1,num2,bit1,bit2):
     p1=int(num1[bit1])%3
     p2=int(num2[bit2])%3
@@ -22,14 +22,43 @@ def rock_paper_scissor(num1,num2,bit1,bit2):
         print("Player two wins!!")
     elif(player_one[p1]=="Scissor" and player_two[p2]=="Paper"):
         print("Player one wins!!")
-player_one={0:'Rock',1:'Paper',2:'Scissor'}
-player_two={0:'Paper',1:'Rock',2:'Scissor'}
-while(1):
-    num1=input("Player one, Enter your choice")
-    num2=input("Player two, Enter your choice")
-    bit1=int(input("Player one, Enter the secret bit position"))
-    bit2=int(input("Player two, Enter the secret bit position"))
-    rock_paper_scissor(num1,num2,bit1,bit2)
-    ch=input("Do you want to continue? y/n")
-    if(ch=='n'):
-        break
+
+# Main Function To Execute Rock Paper Scissor        
+if __name__ == '__main__':        
+    player_one={0:'Rock',1:'Paper',2:'Scissor'}
+    player_two={0:'Paper',1:'Rock',2:'Scissor'}
+    while(1):
+        num1=input("Player one, Enter your choice ")
+        num2=input("Player two, Enter your choice ")
+        bit1=int(input("Player one, Enter the secret bit position "))
+        bit2=int(input("Player two, Enter the secret bit position "))
+        rock_paper_scissor(num1,num2,bit1,bit2)
+        ch=input("Do you want to continue? y/n ")
+        if(ch=='n'):
+            break
+            
+            
+'''
+Output Implementation:
+
+Player one, Enter your choice 123                                                                                               
+Player two, Enter your choice 567                                                                                               
+Player one, Enter the secret bit position 0                                                                                     
+Player two, Enter the secret bit position 1                                                                                     
+Draw
+
+Do you want to continue? y/n y                                                                                                  
+Player one, Enter your choice 012                                                                                               
+Player two, Enter your choice 234                                                                                               
+Player one, Enter the secret bit position 1                                                                                     
+Player two, Enter the secret bit position 2                                                                                     
+Player one wins!!
+
+Do you want to continue? y/n y                                                                                                  
+Player one, Enter your choice 345                                                                                               
+Player two, Enter your choice 012                                                                                               
+Player one, Enter the secret bit position 0                                                                                     
+Player two, Enter the secret bit position 2                                                                                     
+Player one wins!!  
+
+'''
