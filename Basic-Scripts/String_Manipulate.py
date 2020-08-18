e4a431f3f9eaec33e8f70dbe6270dba13f7bcfa0
Ankit Dobhal <dobhal.ankit@protonmail.com>
2020-08-18 05:38:50 -0400

    Delete String_Manipulate.py

--- Basic-Scripts/String_Manipulate.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-print('How are you?')
-feeling = input()
-#feelings = ['great', 'happy', 'sad', 'fine', 'excited']
-#if feeling.lower()=='great':
-    #print('I feel great too!')
-#else:
-    #print('I hope the rest of day is good!')
-print(" I hope you stay "+ feeling + "forever")
