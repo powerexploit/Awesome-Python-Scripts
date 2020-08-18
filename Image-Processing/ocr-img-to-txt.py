d3cc0c175d2d6ac82f3809be19022a36735a91d3
Avinash Kr. Ranjan <ranjan.avinash@hotmail.com>
2020-08-18 10:25:17 +0530

    ✒ Updated ocr-img-to-txt

--- Image-Processing/ocr-img-to-txt.py
+++ Image-Processing/ocr-img-to-txt.py
@@ -1,4 +1,4 @@
-import pytesseract
+import pytesseract 
 from PIL import Image
 
 
@@ -7,10 +7,17 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tessera
 
 
 def convert():
-    img = Image.open(
-        r"E:\Python-Programs\PythonProjects\OCR-Image-To-Text\Img-To-Convt\img1.jpg")
-    text = pytesseract.image_to_string(img)
-    print(text)
-
-
+    #Image to Convert
+    img = Image.open(r"img1.jpg")
+    #sending Image to Pytesseract
+    text = pytesseract.image_to_string(img) 
+    # printing out Text from the Images\
+    print(text)  
+    # If you want to save the output in a file, if not comment out the next two lines
+    # To open a file in write mode
+    newfile = open("newfile.txt", "w+") 
+    # To Write the text in that file
+    newfile.write(text)
+    
+    
 convert()
