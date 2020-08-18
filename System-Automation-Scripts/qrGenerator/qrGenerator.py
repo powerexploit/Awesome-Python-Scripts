aab1c2773914c21095ea3c8e932390d7562c37f7
subhayu99 <balasubhayu99@gmail.com>
2020-08-18 10:11:46 +0530

    qrGenerator

--- System-Automation-Scripts/qrGenerator/qrGenerator.py
+++ System-Automation-Scripts/qrGenerator/qrGenerator.py
@@ -5,15 +5,15 @@ from urllib.parse import urlparse
 import random
 
 if len(argv)==2:
-  url =  str(argv[1])
+	url =  str(argv[1])
 else:
-  url = input("Enter an URL to Encode in QR: ")
+	url = input("Enter an URL to Encode in QR: ")
 
 path = ((urlparse(url)).netloc)
 image = qrcode.make(url) 
 
 if os.path.exists(f"{path}.png"):
-  path = path + str(random.randint(0,1000))
+	path = path + str(random.randint(0,10000))
 
 image.save(f"{path}.png", "PNG")
 file_path = os.path.abspath(path)
