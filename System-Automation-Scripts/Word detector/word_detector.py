66892b681c5f2bba4907908ac47433a55804dcfe
Aditya Jetely <ajetely@gmail.com>
2020-08-18 08:09:22 +0530

    Added Readme and created a separate folder

--- System-Automation-Scripts/Word detector/word_detector.py
+++ System-Automation-Scripts/Word detector/word_detector.py
@@ -1,17 +1,3 @@
-"""[This script is used to detect whether a given word is present in a list of files.
-    All the files which contain the specified word gets stored in a seperate folder.
-
-It takes two inputs - 
-1.Path of the directory which contains the files.
-
-[Sample path - "C:/Users/<Your System Name>/Desktop/<Target Folder>/"]
-
-2.Word that you want to detect in these files.
-[Sample word - "awesome"]
-
-]
-"""
-
 import os
 import shutil
 
