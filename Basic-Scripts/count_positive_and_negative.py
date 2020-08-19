917b198a1d04576b36fd89a173fa14b2918868af
Kapil Kumar <56160115+kapilkumar2001@users.noreply.github.com>
2020-08-19 09:50:50 +0530

    Update count_positive_and_negative.py

--- Basic-Scripts/count_positive_and_negative.py
+++ Basic-Scripts/count_positive_and_negative.py
@@ -2,16 +2,16 @@
   
 # list of numbers 
 list1 = [10, -21, 4, -45, 66, -93, 1] 
-  
-pos_count, neg_count = 0, 0
+
+positive_count, negative_count = 0, 0
   
 # iterating each number in list 
 for num in list1:     
     # checking condition 
     if num >= 0: 
-        pos_count += 1
+        positive_count += 1
     else: 
-        neg_count += 1
+        negative_count += 1
           
-print("Positive numbers in the list: ", pos_count) 
-print("Negative numbers in the list: ", neg_count) 
+print("Positive numbers in the list: ", positive_count) 
+print("Negative numbers in the list: ", negative_count) 
