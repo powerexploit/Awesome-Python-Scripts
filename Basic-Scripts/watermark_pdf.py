e07e691dd3f32ca49a53005e9c3dcaaa4989bd54
Arun Kumar G <arunkumarakansha@gmail.com>
2020-08-20 12:40:12 +0530

    Added changes to watermark PDF as per PR review

--- Basic-Scripts/watermark_pdf.py
+++ Basic-Scripts/watermark_pdf.py
@@ -7,20 +7,24 @@ Intsall module using command 'pip install PyPDF2'
 '''
 import PyPDF2
 
-# Initializing PDF file reader object for Source PDF
-pdf_file_reader = PyPDF2.PdfFileReader(open('./PDF_Docs/give-and-take-BOOK.pdf','rb'))
+SOURCE_PDF      = "<PROVIDE THE SOURCE PDF PATH HERE >"
+WATERMARK_PDF   = "< PROVIDE THE WATERMARK PDF PATH HERE >"
+DESTINATION_PDF = "< PROVIDE THE DESTINATION PATH HERE FOR STORING FINAL PDF >"
 
-# Initializing Water mark PDF file reader object which has only water mark in first page
-watermark_file_reader = PyPDF2.PdfFileReader(open('./PDF_Docs/Confidential.pdf','rb'))
+# Initializing PDF file reader object for Source PDF
+pdf_file_reader = PyPDF2.PdfFileReader(open(SOURCE_PDF,'rb'))
+
+# Initializing Water mark PDF file reader object which has only Water mark in first page
+watermark_file_reader = PyPDF2.PdfFileReader(open(WATERMARK_PDF,'rb'))
 
 watermark = watermark_file_reader.getPage(0) #To get first page of the PDF which has water mark
 
 writer = PyPDF2.PdfFileWriter()  #Initilaizes PDF writer object
 
 for each_page in range(pdf_file_reader.getNumPages()):
-    page = pdf_file_reader.getPage(each_page) # To each page in the PDF
+    page = pdf_file_reader.getPage(each_page) # To get each page in the PDF
     page.mergePage(watermark)  # Merges/Adds the water mark to each page
     writer.addPage(page)  # Writes the page to writer object
 
-with open('./Processed_PDF_Docs/Book.pdf','wb') as file:
-    writer.write(file) # Final pdf with water mark is stored here
+with open(DESTINATION_PDF,'wb') as file:
+    writer.write(file) # Final pdf with water mark is stored in destination path
