5cd8472a05393f5ee1620af18c6b9bd0dc205aeb
namrun <nowitsharder@gmail.com>
2020-08-20 12:34:29 +0530

    Changes made to the script

--- Image-Processing/Image_filters_using_convolution/3D_convolve.py
+++ Image-Processing/Image_filters_using_convolution/3D_convolve.py
@@ -17,6 +17,7 @@ height = 360
 r_kernel = np.array([0.11 , 0.11 , 0.11 , 0.11 , 0.11 , 0.11 , 0.11 , 0.11 , 0.11]).reshape(3,3)
 g_kernel = np.array([0.11 , 0.11 , 0.11 , 0.11 , 0.11 , 0.11 , 0.11 , 0.11 , 0.11]).reshape(3,3)
 '''
+
 print("Enter 9 values for each of the streams")
 b_kernel = np.array(list(map(int, input("Enter the kernel to be applied to the blue stream").split()))).reshape(3,3)
 r_kernel = np.array(list(map(int, input("Enter the kernel to be applied to the red stream").split()))).reshape(3,3)
@@ -25,13 +26,13 @@ g_kernel = np.array(list(map(int, input("Enter the kernel to be applied to the g
 #Image is read and resized
 
 img = cv2.imread('path to image')
-img = cv2.resize(img, (width , height))
+img = cv2.resize(img, (width, height))
 
 #The pixels are extracted from the image
 pixels = []
 for i in range(height):
       for j in range(width):
-              pixels.append(img[i,j])
+              pixels.append(img[i, j])
 
               #The RGB streams are stored in different arrays to facilitate the process of convolution
               b = []
@@ -44,20 +45,21 @@ for i in range(height):
                 r.append(i[2])
 
 #Each of the streams are resized
+
 r = np.array(r).reshape(height, width)
 b = np.array(b).reshape(height, width)
 g = np.array(g).reshape(height, width)
 
 #function to apply convolution or multiply kernels
-def convolute(arr , height , width ,kernel):
+def convolute(arr, height, width, kernel):
       convoluted_matrix = []
-      for i in range(1,height-2):
-          for j in range(1,width-2):
-            temp = arr[i:i+3 , j:j+3]
+      for i in range(1, height-2):
+          for j in range(1, width-2):
+            temp = arr[i:i+3, j:j+3]
             prod = np.multiply(temp,kernel)
             convoluted_matrix.append(np.sum(prod))
 
-      convoluted_matrix = (np.array(convoluted_matrix).reshape(height-3,width-3))
+      convoluted_matrix = (np.array(convoluted_matrix).reshape(height-3, width-3))
       return(convoluted_matrix)
 
 r_convol = convolute(r, height, width, r_kernel)
@@ -72,11 +74,9 @@ combine = np.zeros([height, width, 3], dtype=np.uint8)
 
 for i in range(height):
       for j in range(width):
-            combine[i ,j] = [int(r_convol[i][j]) , int(g_convol[i][j]) , int(b_convol[i][j])]
+            combine[i, j] = [int(r_convol[i][j]), int(g_convol[i][j]), int(b_convol[i][j])]
 
 
 #The 3 matrices or streams are combined, and stored
 img = Image.fromarray(combine)
 img.save('Funkyfilter.jpg')
-
-
