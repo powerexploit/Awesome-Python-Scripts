7001a92ab0146e912b5ed1f1e9fe01ebf7a60517
Mayank goel <mayankgoyal662@gmail.com>
2020-08-20 15:08:09 +0530

    Update day_of_date.py

--- Basic-Scripts/day_of_date.py
+++ Basic-Scripts/day_of_date.py
@@ -1,24 +1,38 @@
-def dayofweek(d, m, y): 
-    t = [ 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 ] 
-    y -= m < 3
-    k=( y + int(y / 4) - int(y / 100) + int(y / 400) + t[m - 1] + d) % 7
-    if k==1:
-        d='Monday'
-    elif k==2:
-        d='Tuesday'
-    elif k==3:
-        d='Wednesday'
-    elif k==4:
-        d='Thursday'
-    elif k==5:
-        d='Friday'
-    elif k==6:
-        d='Saturday'
-    else:
-        d='Sunday'
-    return d
+#Function to calculate day of the week
+def dayofweek(
+        d, m, y):
+    
+    t = [
+        0, 3, 2,
+        5, 0, 3,
+        5, 1, 4,
+        6, 2, 4
+        ]
+    
+    y = y - (m < 3)
+    k=(
+        y
+        + int(y / 4)
+        - int(y / 100)
+        + int(y / 400)
+        + t[m - 1]
+        + d
+       ) % 7
+    
+    switcher={
+        1:'Monday',
+        2:'Tuesday',
+        3:'Wednesday',
+        4:'Thursday',
+        5:'Friday',
+        6:'Saturday',
+        7:'Sunday'
+        }
+    
+    return switcher.get(k,'Invalid')
 
   
-
-#day = dayofweek(24, 4, 2020) 
-#print(day)
+#Driver Program
+if __name__=='__main__':
+    day=dayofweek(20,8,2020)
+    print(day)
