5c5c5fe05a3805d15e37dc277f6eaa3f463efc0e
RICHA BHARTI <50147461+richabh456@users.noreply.github.com>
2020-08-21 22:37:07 +0530

    I have updated the required changes.

--- Basic-Scripts/guessinggame.py
+++ Basic-Scripts/guessinggame.py
@@ -1,18 +1,28 @@
-
 import random
-print("Number guessing game")
-number = random.randint(1, 99)
-chances = 0
-print("Guess a number (between 1 and 99):")
-while chances < 8:
-    guess = int(input())
-    if guess == number:
-        print("Hooray YOU WON!!!")
-        break
-    elif guess < number:
-        print("Your guess was too low: Guess a number higher than", guess)
-    else:
-        print("Your guess was too high: Guess a number lower than", guess)
-    chances += 1
-if not chances < 8:
-    print("YOU LOSE!!! The number is", number)
+def start(limit, chances):
+    number = random.randint(1, limit)
+    win = False
+    while chances > 0:
+        guess = int(input())
+        if guess == number:
+            print("HOORAY YOU WON!!!")
+            win = True
+            break
+        elif guess < number:
+            print("Your guess was too low: Guess a number higher than", guess)
+            win = False
+        else:
+            print("Your guess was too high: Guess a number lower than", guess)
+            win = False
+        chances -= 1
+
+    if win is not True:
+        print("YOU LOSE!!! The number is", number)
+
+
+if __name__ == "__main__":
+    print("Number guessing game")
+    limit = int(input("Enter range of number: "))
+    chances = int(input("Enter the number of chances: "))
+    print("Guess a number (between 1 and your range): ")
+    start(limit, chances)
