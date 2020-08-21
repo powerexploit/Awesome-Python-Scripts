4809e668cc716c66c00ca78aeb9f7d9b60e67bdb
Amandeep <amanxyx@gmail.com>
2020-08-21 21:05:37 +0530

    changes requested

--- Basic-Scripts/advanceCalculator.py
+++ Basic-Scripts/advanceCalculator.py
@@ -1,39 +1,47 @@
-#importing the regexp module in my script
+# Script of Advance Calculator using Python using regexp module
+
+# importing the regexp module
 import re
 
-#salutation users
-print("Welcome! I am your Calculator")
-print("Type 'quit' to terminate\n")
+# salutation and providing termination condition.
+print("Welcome! I am your Advance Calculator.")
+print("Type 'quit' to terminate.\n")
 
 #declaring global variables
-previous = 0
-run=True
+previous_value = 0
+flag = True
 
-#defining non parametrized function
-def performMath():
-    global run 
-    global previous
-    equn = ""
-    if previous == 0:
-        equn = input("Give me something to calculate:")
+# Defining non parametrized function to perform all the operations. 
+def calculate():
+    global flag 
+    global previous_value 
+
+    equation = ""
+    
+    # Taking input from the user.
+    if previous_value == 0:
+        equation = input("Give me something to calculate:")
     else:
-        equn = input(str(previous))
+        equation = input(str(previous_value))
 
-    if equn == 'quit':
+    # Termination step.
+    if equation == 'quit':
         print("You exit.")
         print("See you next time.")
-        run = False
+        flag = False
     else:
-        equn = re.sub('[a-zA-z,.:()" "]', '', equn)
-        # removing if any words are entered
+        equation = re.sub('[a-zA-z,.:()" "]', '', equation)
+        # For security point of view here we eliminate all the letters and keep only numerical characters, if entered by the user.
 
-        if previous == 0:
-            previous = eval(equn)
+        # Calculating the results.
+        if previous_value == 0:
+            previous_value = eval(equation)
         else:
-            previous = eval(str(previous) + equn)
+            previous_value = eval(str(previous_value) + equation)
 
-#loop with calling the function
-while run:
-    performMath()
+# Calling the function within the infinite loop.
+# On execution will continue to calculate until the user enters 'quit' .
+while flag:
+    calculate()
 
-#end of script
+#end of the script
