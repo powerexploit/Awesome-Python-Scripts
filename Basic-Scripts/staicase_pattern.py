27fd81a069f4f1b66ad49dfc04ab580111fe3588
Adityanagraj <42292430+Adityanagraj@users.noreply.github.com>
2020-08-22 19:22:27 +0530

    Apply suggestions from code review
    Co-authored-by: Sarath Kaul <kaul.sarath@gmail.com>

--- Basic-Scripts/staicase_pattern.py
+++ Basic-Scripts/staicase_pattern.py
@@ -9,6 +9,10 @@ Which prints the staircase pattern
 def staircase(height):
    """
    This program prints the staircase pattern for the given height (dynamically)
+   Params:
+       height: height of the staircase as entered by user
+   Returns:
+       None
    """
    for i in range(1,height+1):
        print(' '*(height-i) + '#'*i)
