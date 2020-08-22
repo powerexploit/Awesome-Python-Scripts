487b8db0762faefb4ba53638585265314284b67b
Prasad V Patil <69467300+prasadvpatil@users.noreply.github.com>
2020-08-22 10:45:31 +0530

    Update quad_equation.py

--- Basic-Scripts/quad_equation.py
+++ Basic-Scripts/quad_equation.py
@@ -1,25 +1,37 @@
 # Python Program to find roots of a Quadratic Equation
 # Solve the quadratic equation ax**2 + bx + c = 0
-# import complex math module
-import cmath
+
+
+import cmath  # import complex math module
+
+def findRoots( a, b, c):
+    """
+    If a is 0 then equation is not quadratic but linear,
+    So if user enters value of a as zero then its invalid.
+    """
+    if a == 0:
+        print("Invalid")
+        return -1
+    discriminant = (b * b) - (4 * a * c)
+    if(discriminant > 0):
+        root_1 = (-b + math.sqrt(discriminant) / (2 * a))
+        root_2 = (-b - math.sqrt(discriminant) / (2 * a))
+        print("Two Distinct Real Roots Exists: ")
+        print("root1 = %.2f and root2 = %.2f" % (root_1, root_2))
+    elif(discriminant == 0):
+        root_1 = root_2 = -b / (2 * a)
+        print("Two Equal and Real Roots Exists: ")
+        print("root1 = %.2f and root2 = %.2f" % (root_1, root_2))
+    elif(discriminant < 0):
+        root_1 = root_2 = -b / (2 * a)
+        imaginary = math.sqrt(-discriminant) / (2 * a)
+        print("Two Distinct Complex Roots Exists: ")
+        print("r1=%.2f+%.2f and r2=%.2f-%.2f" % (root_1, imaginary, root_2, imaginary))
+
+# Main Program       
+
 a = int(input("Please Enter a Value of a Quadratic Equation : "))
 b = int(input("Please Enter b Value of a Quadratic Equation : "))
 c = int(input("Please Enter c Value of a Quadratic Equation : "))
 
-discriminant = (b * b) - (4 * a * c)
-
-if(discriminant > 0):
-    r1 = (-b + math.sqrt(discriminant) / (2 * a))
-    r2 = (-b - math.sqrt(discriminant) / (2 * a))
-    print("Two Distinct Real Roots Exists: ")
-    print("root1 = %.2f and root2 = %.2f" % (r1, r2))
-elif(discriminant == 0):
-    r1 = r2 = -b / (2 * a)
-    print("Two Equal and Real Roots Exists: ")
-    print("root1 = %.2f and root2 = %.2f" % (r1, r2))
-elif(discriminant < 0):
-    r1 = r2 = -b / (2 * a)
-    imaginary = math.sqrt(-discriminant) / (2 * a)
-    print("Two Distinct Complex Roots Exists: ")
-    print("r1=%.2f+%.2f and r2=%.2f-%.2f" % (r1, imaginary, r2, imaginary))
-
+findRoots(a, b, c)
