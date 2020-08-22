18f7203aafb5af76a805bee036f309932ed96cfc
sujal111 <62564807+sujal111@users.noreply.github.com>
2020-08-22 06:55:12 -0700

    Integer to Roman Numeral
    Integer to Roman Numeral in Python

---
+++ Basic-Scripts/integetToroman.py
@@ -0,0 +1,27 @@
+class py_solution:
+    def int_to_Roman(self, num):
+        val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
+            ]
+        syb = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+            ]
+        roman_num = ''
+        i = 0
+        while  num > 0:
+            for _ in range(num // val[i]):
+                roman_num += syb[i]
+                num -= val[i]
+            i += 1
+        return roman_num
+
+
+print(py_solution().int_to_Roman(1))
+print(py_solution().int_to_Roman(4000))
+
