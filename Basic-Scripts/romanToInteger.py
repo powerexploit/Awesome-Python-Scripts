495a784e15dc7044f78c4954fabfc21a7e0fee89
sujal111 <62564807+sujal111@users.noreply.github.com>
2020-08-22 06:56:30 -0700

    Roman To Integer
    Roman numeral to Integer in Python

---
+++ Basic-Scripts/romanToInteger.py
@@ -0,0 +1,14 @@
+class py_solution:
+    def roman_to_int(self, s):
+        rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        int_val = 0
+        for i in range(len(s)):
+            if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
+                int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
+            else:
+                int_val += rom_val[s[i]]
+        return int_val
+
+print(py_solution().roman_to_int('MMMCMLXXXVI'))
+print(py_solution().roman_to_int('MMMM'))
+print(py_solution().roman_to_int('C'))
