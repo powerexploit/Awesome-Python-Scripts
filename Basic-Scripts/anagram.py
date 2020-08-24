c94b88a9dc5dc997156f8a032cadabad2c01d455
GULSHAN BARAIK <45667577+gulshanbaraik01@users.noreply.github.com>
2020-08-24 12:15:14 +0530

    Update and rename anagram3.py to anagram.py

---
+++ Basic-Scripts/anagram.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  6 19:25:33 2019
+
+@author: Gulshan
+"""
+
+'''def areAnagram(string1, string2):
+    if sorted(string1)==sorted(string2):
+        return 1
+    else:
+        return 0
+    
+
+
+string1 = 'geeksforgeeks'
+string2 = 'forgeeksgeeks'
+if (areAnagram(string1, string2)):
+    print('strings are anagram')
+else:
+    print('strings are not anagram')'''
+    
+from collections import Counter
+
+def areAnagram(string1, string2):
+    return Counter(string1) == Counter(string2)
+
+
+string1 = 'geeksforgeeks'
+string2 = 'forgeeksgeeks'
+print(areAnagram(string1, string2))
+ 
