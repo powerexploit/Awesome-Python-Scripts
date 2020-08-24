c09c4ba2d26bbc35430749108697ea451213b420
Arju Aman <33257286+arjuaman@users.noreply.github.com>
2020-08-24 21:14:30 +0530

    Added docstring and it now takes input from user

--- Basic-Scripts/selection_sort.py
+++ Basic-Scripts/selection_sort.py
@@ -1,4 +1,5 @@
 def selecsort(lst):
+    ''' It takes a list as input from the user and returns it's sorted version. '''
     for i in range(len(lst)):
         minpos=i
         for j in range(i,len(lst)):
@@ -6,6 +7,13 @@ def selecsort(lst):
                 minpos=j
         (lst[i],lst[minpos])=(lst[minpos],lst[i])
 
-    for item in lst:
-        print(item)
-selecsort([1,5,87,2,-56,89])
+    return lst;
+        
+n = int(input("Enter the number of elements to sort: "))
+input_list = []
+for element in range(0,n):
+    cin = int(input("Enter the element: "))
+    input_list.append(cin)
+
+sorted_list = selecsort(input_list)
+print(sorted_list)
