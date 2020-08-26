9ec0c746429c873962814b56049c1d1567c9ee22
RutujaD182 <dhanawaderutuja1002@gmail.com>
2020-08-26 16:12:20 +0530

    added required changes

--- Web-Scraping/IMDB_TopMovies/imdb_topmovies.py
+++ Web-Scraping/IMDB_TopMovies/imdb_topmovies.py
@@ -12,13 +12,13 @@ import pandas as pd
 url = 'http://www.imdb.com/chart/top'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html')
-
+ 
 movies = soup.select('td.titleColumn')
 stars = [a.attrs.get('title') for a in soup.select('td.titleColumn a')]
-ratings = [b.attrs.get('data-value') for b in soup.select('td.posterColumn span[name=ir]')]
-
+ratings = [round(float(b.attrs.get('data-value')),1) for b in soup.select('td.posterColumn span[name=ir]')]
+ 
 imdb = []
-
+ 
 # Store each item into dictionary (data), then put those into a list (imdb)
 for index in range(0, len(movies)):
     movie_string = movies[index].get_text()
@@ -31,14 +31,12 @@ for index in range(0, len(movies)):
             "rating": ratings[index],
             }
     imdb.append(data)
-
+ 
 # CREATING A DATAFRAME
 df = pd.DataFrame(imdb)
-
+df.index = df.index.rename('S.No')
 #copy data frame in to CSV file
 #csv file has been created in current working directory
-
 df.to_csv('imdb.csv')
-read=pd.read_csv('/content/imdb.csv') 
-
+#read=pd.read_csv('/content/imdb.csv') 
 #print(read)
