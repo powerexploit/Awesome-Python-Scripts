1b3734e3a9a5dc9a588bddfebaae6a00d131f788
nutanaarohi123 <nutan7040308@gmail.com>
2020-08-29 21:24:48 +0530

    Python_Numbers

---
+++ Basic-Scripts/Python_Numbers.py
@@ -0,0 +1,24 @@
+# Python_Numbers
+# There are three types of number of in Python
+# int,float,complex
+
+print("Input :")
+x = 15  # int
+y = 14.5  # float
+z = 1j  # complex
+print("x = 15, y = 14.5, z = 1j")
+
+# type() function is used to determine the type of variable
+print("Output :")
+print(type(x))
+print(type(y))
+print(type(z))
+
+"""
+ Input :
+ x = 15, y = 14.5, z = 1j
+ Output :
+ <class 'int'>
+ <class 'float'>
+ <class 'complex'>
+"""
