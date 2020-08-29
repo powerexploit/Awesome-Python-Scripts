7f629417a9aaaaa05f9718a0487e27ae0c9166c8
GULSHAN BARAIK <45667577+gulshanbaraik01@users.noreply.github.com>
2020-08-29 19:51:51 +0530

    Update anagram.py

--- Basic-Scripts/anagram.py
+++ Basic-Scripts/anagram.py
@@ -1,33 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Apr  6 19:25:33 2019
+Created on Sat Aug 13 19:25:33 2020
 
 @author: Gulshan
 """
 
-'''def areAnagram(string1, string2):
-    if sorted(string1)==sorted(string2):
-        return 1
-    else:
-        return 0
-    
 
-
-string1 = 'geeksforgeeks'
-string2 = 'forgeeksgeeks'
-if (areAnagram(string1, string2)):
-    print('strings are anagram')
-else:
-    print('strings are not anagram')'''
     
 from collections import Counter
 
-def checkAnagram(string1, string2):
+def check_anagram(string1, string2):
     return Counter(string1) == Counter(string2)
 
 
 if __name__ == '__main__':
-    string1 = 'geeksforgeeks'
-    string2 = 'forgeeksgeeks'
-    print(checkAnagram(string1, string2))
+    string1 = input("Enter First String: ")
+    string2 = input("Enter Second String: ")
+    if check_anagram(string1, string2):
+        print("Strings are Anagram!")
+    else:
+        print("Strings are not Anagram!")
  
