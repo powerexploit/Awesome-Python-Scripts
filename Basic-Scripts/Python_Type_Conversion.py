e5cb2f909286f23c293aa93cd1ac81a2a226f5d6
nutanaarohi123 <nutan7040308@gmail.com>
2020-08-29 22:21:26 +0530

    Type_Conversion

---
+++ Basic-Scripts/Python_Type_Conversion.py
@@ -0,0 +1,33 @@
+#Python Type_Conversion
+
+#int() constructor function  convert the input(integer,float,string) into integer type 
+a = int(4)         # value of a will be 4
+b = int(4.3)       # value of b will be 4
+c = int("5")       # value of c will be 5
+print(a)
+print(b)
+print(c)
+
+#float() constructor function convert the input(integer,float,string) into float type
+x = float(4)       #value of x will be 4.0
+y = float(8.3)     #value of y will be 8.3
+z = float("8")     #value of z will be 8.0
+print(x)
+print(y)
+print(z)
+
+#str() constructor function convert the input(integer,float,string) into String type
+w = str(4)          #value of w will be '4'
+x = str(5.3)        #value of x will be '5.3'
+y = str("aarohi")   #value of y will be 'aarohi'
+print(w)
+print(x)
+print(y)
+
+complex() constructor function convert the input(integer,float) into complex type
+i = complex(1)     #value of i will be 1+0j
+j = complex(5.3)   #value of j will be 5.3+0j
+k = complex(8j)    #value of k will be 8j
+print(i)
+print(j)
+print(k)
