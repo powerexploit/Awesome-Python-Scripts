9abbfd2924a9572db5725f996804e349ecd284b5
Arju Aman <33257286+arjuaman@users.noreply.github.com>
2020-08-30 22:54:31 +0530

    Added docstring and user input functionality

--- Basic-Scripts/karatsuba-mul.py
+++ Basic-Scripts/karatsuba-mul.py
@@ -2,33 +2,12 @@ import numpy as np
 import sys
 print(sys.getrecursionlimit())
 sys.setrecursionlimit(9000)
-#x=3141592653589793238462643383279502884197169399375105820974944592
-#y=2718281828459045235360287471352662497757247093699959574966967627
-#len=64
-#x=5678
-#y=1234
-#l=4
-#b=x%100
-#a=(x-b)/100
-#d=y%100
-#c=(y-d)/100
-#ans=(a*c)*(10**4)+(b*d)+(a*d+b*c)*100
-#print(ans)
 
 
-#b=x%(10**(len/2))
-#print(b)
-#a=(x-b)/(len/2)
-#x=(10**(len/2))*a+b
-#d=y%(10**(len/2))
-#print(d)
-#c=(y-d)/(len/2)
-#y=(10**(len/2))*c+d
-#ans=(a*c)*(10**len)+(b*d)+(a*d+b*c)*(10**(len/2))
-#print(ans)
-#for equal digits in x and y
 def karatsuba(x,y):
-    #len1=len(x)
+    ''' The function takes two numbers in string format. 
+    The Karatsuba algorithm is a fast multiplication algorithm, using Divide and Conquer approach.
+    It reduces the multiplication of two n-digit numbers to at most single-digit multiplications in general. '''
     count=0
     m=int(x)
     len1=0
@@ -60,7 +39,7 @@ def karatsuba(x,y):
         f3=karatsuba(a+b,c+d)
         return (f1*(10**len1)+f2+(f3-f1-f2)*(10**(len1/2)))
 
-var1=str(3141592653589793238462643383279502884197169399375105820974944592)
-var2=str(2718281828459045235360287471352662497757247093699959574966967627)
+var1 = input('Enter number 1')
+var2 = input('Enter number 2')
 print(karatsuba(var1,var2))
-#print(karatsuba(1200,1400))
+
