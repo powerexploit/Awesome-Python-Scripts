fd3dd494209dd45894355ae18bc920c0beda6d6f
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2020-09-05 23:58:16 +0530

    Updated detecting_geometrical_shapes.py
    Removed extra new lines from the code.

--- Image-Processing/Detecting Contours/detecting_geometrical_shapes.py
+++ Image-Processing/Detecting Contours/detecting_geometrical_shapes.py
@@ -4,10 +4,8 @@ import cv2
 img = cv2.imread('shapes.PNG')
 imgGry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-
 ret , thrash = cv2.threshold(imgGry, 240 , 255, cv2.CHAIN_APPROX_NONE)
 contours , hierarchy = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
 
 for contour in contours:
     approx = cv2.approxPolyDP(contour, 0.01* cv2.arcLength(contour, True), True)
@@ -21,6 +19,7 @@ for contour in contours:
         x, y , w, h = cv2.boundingRect(approx)
         aspectRatio = float(w)/h
         print(aspectRatio)
+        
         if aspectRatio >= 0.95 and aspectRatio < 1.05:
             cv2.putText(img, "square", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
             
@@ -39,5 +38,3 @@ for contour in contours:
 cv2.imshow('shapes', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
